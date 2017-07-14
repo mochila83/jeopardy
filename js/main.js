c058a00bf43e9fafd272e90beacbf1579ba2534c
Jessica OBrien <obrien.jessica6@gmail.com>
2017-07-14 13:09:35 -0400

    added a for loop

---
+++ js/main.js
@@ -0,0 +1,207 @@
+var questions = [{
+	questionId: 'a1',
+	category: 'The Arts',
+	question: 'Rhapsody in Blue is a musical composition by which American composer? A. Franz Liszt B. George Gershwin C. Sergei Rachmaninoff D. Claude Debussy'
+	answer: 'B. George Gershwin',
+	points: 100,
+
+}, 
+{
+	questionId: 'a2',
+	category: 'The Arts',
+	question: 'Which 20th century painter made their permanent home in New Mexico and painted the desert terrian? A. Salvador Dali B. Frida Kahlo C. Georgia O\'Keefe D. Jackson Pollock',
+	answer: 'C. Georgia O\'Keefe',
+	points: 200,
+
+	},
+{
+
+
+	category: 'The Arts',
+	question: 'The Bolshoi Ballet is an internationally renowned ballet company founded in which country? A. Russia B. Poland C. Ukraine D. United States',
+	answer: 'A. Russia',
+	points: 300,
+
+},
+{
+
+
+	category: 'The Arts',
+	question: 'When was "The Golden Age of Illustration"? A. 1750-1800 B. 1880-1920 C. 1930-1940 D. 1600-1650',
+	answer: 'B. 1880-1920',
+	points: 400,
+
+},
+}
+
+category: 'The Arts',
+question: 'Which country was the paleolithic figurine "The Venus of Willendorf" found? A. China B. Romania C. Austria D. France',
+answer: 'C. Austria',
+points: 500,
+
+},
+}
+
+category: 'World Leaders',
+question: 'Who is the current Prime Minister of Canada? A. Theresa May B. Emmanuel Macron C. Justin Trudeau D. Angela Merkel',
+answer: 'C. Justin Trudeau',
+points: 100,
+
+},
+}
+
+category: 'World Leaders',
+question: 'Who is the current Prime Minister of Israel? A. Benjamin Netanyahu B. Ariel Sharon C. Yitzhak Rabin D. Yasser Arafat',
+answer: 'A. Benjamin Netanyahu',
+points: 200,
+
+},
+}
+
+category: 'World Leaders',
+question: 'Which Egyptian politician was the Secretary-General of the United Nations from 1992-1996? A. Anwar Sadat B. Gamal Abdel Nasser C. Abdel Fattah el-Sisi D. Boutros Boutros-Ghali',
+answer: 'D. Boutros Boutros-Ghali',
+points: 300,
+
+},
+}
+
+category: 'World Leaders',
+question: 'The 1971 Ugandan coup d\'etat was led by which military general who later became President? A. Idi Amin B. Mummar Gaddafi C. Milton Obote D. Yoweri Mudseveni',
+answer: 'A. Idi Amin',
+points: 400,
+
+},
+}
+
+category: 'World Leaders',
+question: 'Which Tunisian President was ousted during the "Arab Spring"? A. Hosni Mubarak B. Habib Bourguiba C. Zine El Abidine Ben Ali D. Beji Caid Essebsi',
+answer: 'C. Zine El Abidine Ben Ali',
+points: 500,
+
+},
+}
+
+category: 'Film Directors',
+question: 'Which director filmed the 1972 film "The Godfather"? A. Michael Bay B. Francis Ford Coppola C. Ron Howard D. Orson Welles',
+answer: 'B. Francis Ford Coppola',
+points: 100,
+
+},
+}
+
+category: 'Film Directors',
+question: '"E.T. the Extra-Terrestrial was directed by which American film maker? A. Steven Speilberg B. James Cameron C. Tim Burton D. George Lucas',
+answer: 'A. Steven Speilberg',
+points: 200,
+
+},
+}
+
+category: 'Film Directors',
+question: '"2001: A Space Odyssey" was made by which American film maker? A. Martin Scorsese B. Robert Altman C. Quentin Tarantino D. Stanley Kubrick',
+answer: 'D. Stanley Kubrick',
+points: 300,
+
+},
+}
+
+category: 'Film Directors',
+question: 'Which Italian film maker known for his fantasy and unique style directed the film "La Dolce Vita"? A. Roberto Rossellini B. Michelangelo Antonioni C. Federico Fellini D. Bernardo Bertolucci',
+answer: 'C. Federico Fellini',
+points: 400,
+
+},
+}
+
+category: 'Film Directors',
+question: 'The movie "Dreams" was the last film of which famous Japanese director? A. Takashi Miike B. Akira Kurosawa C. Kenji Mizoguchi D. Shohei Imamura',
+answer: 'B. Akira Kurosawa',
+points: 500,
+
+},
+}
+
+category: 'Mythology',
+question: 'Which Greek goddess was knowns as goddess of beauty, love, desire, and pleasure? A. Andromeda B. Helen of Troy C. Athena D. Aphrodite',
+answer: 'D. Aphrodite',
+points: 100,
+
+},
+}
+
+category: 'Mythology',
+question: 'Which Greek god was known as the god of boundaries, travel, communication, trade, language, and writing? A. Hermes B. Hades C. Apollo D. Zeus',
+answer: 'A. Hermes',
+points: 200,
+
+},
+}
+
+category: 'Mythology',
+question: 'Which Egyptian deity\'s prominent role was maintaining the universe? A. Isis B. Ma\'at C. Anubis D. Thoth',
+answer: 'Thoth',
+points: 300,
+
+},
+}
+
+category: 'Mythology',
+question: 'Which Roman god is known as the god of the sky and thunder? A. Jupiter B. Mars C. Vulcan D. Mercury',
+answer: 'A. Jupiter',
+points: 400,
+
+},
+}
+
+category: 'Mythology',
+question: 'Which Roman god is depicted as having two-faces and is known as the god of beginnings, gates, transitions, time, duality, passages, and endings? A. Minerva B. Ceres C. Janus D. Vesta',
+answer: 'C. Janus',
+points: 500,
+
+},
+}
+
+category: 'Philosophy',
+question: 'Which Greek philosopher wrote "The Republic"? A. Plato B. Aristotle C. Socrates D. Plutarch',
+answer: 'A. Plato',
+points: 100,
+
+},
+}
+
+category: 'Philosophy',
+question: 'The book "Thus Spoke Zarathustra" was written by which philosopher? A. Immanuel Kant B. Rene Descartes C. Friedrich Nietzche D. Soren Kierkegaard',
+answer: 'C. Friedrich Nietzche',
+points: '200',
+
+},
+}
+
+category: 'Philosophy',
+question: 'Which French philosopher is known for their work in post-structuralism and postmodern philosophy? A. Simone de Beauvoir B. Voltaire C. Jean-Paul Sartre D. Jacques Derrida',
+answer: 'D. Jacques Derrida',
+points: 300,
+
+},
+}
+
+category: 'Philosophy',
+question: 'Which artist is known for his painting "The School of Athens" which depicts nearly every great Greek philosopher? A. El Greco B. Michelangelo C. Caravaggio D. Raphael',
+answer: 'D. Raphael',
+points: 400,
+
+},
+}
+
+category: 'Philosophy',
+question: 'Which philosopher who is known primarily for his work in logic, mathematics, and linguistics and published the book "Tractatus Logico-Philosophicus"? A. Karl Marx B. Friedrich Engels C. Ludwig Wittgenstein D. Bertrand Russel',
+answer: 'C. Ludwig Wittgenstein',
+points: 500,
+
+},
+}
+
+]  
+
+
