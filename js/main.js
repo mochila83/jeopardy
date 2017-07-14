5ad34826031b88c545b8ca405f9bce9c0caac51b
Jessica OBrien <obrien.jessica6@gmail.com>
2017-07-14 13:15:46 -0400

    fixed syntax in javascript

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var questions = [{
 	points: 100,
 
 }, 
+
 {
 	questionId: 'a2',
 	category: 'The Arts',
@@ -23,6 +24,7 @@ var questions = [{
 	points: 300,
 
 },
+
 {
 
 
@@ -32,7 +34,8 @@ var questions = [{
 	points: 400,
 
 },
-}
+
+{
 
 category: 'The Arts',
 question: 'Which country was the paleolithic figurine "The Venus of Willendorf" found? A. China B. Romania C. Austria D. France',
@@ -40,7 +43,8 @@ answer: 'C. Austria',
 points: 500,
 
 },
-}
+
+{
 
 category: 'World Leaders',
 question: 'Who is the current Prime Minister of Canada? A. Theresa May B. Emmanuel Macron C. Justin Trudeau D. Angela Merkel',
@@ -48,7 +52,8 @@ answer: 'C. Justin Trudeau',
 points: 100,
 
 },
-}
+
+{
 
 category: 'World Leaders',
 question: 'Who is the current Prime Minister of Israel? A. Benjamin Netanyahu B. Ariel Sharon C. Yitzhak Rabin D. Yasser Arafat',
@@ -56,7 +61,8 @@ answer: 'A. Benjamin Netanyahu',
 points: 200,
 
 },
-}
+
+{
 
 category: 'World Leaders',
 question: 'Which Egyptian politician was the Secretary-General of the United Nations from 1992-1996? A. Anwar Sadat B. Gamal Abdel Nasser C. Abdel Fattah el-Sisi D. Boutros Boutros-Ghali',
@@ -64,7 +70,8 @@ answer: 'D. Boutros Boutros-Ghali',
 points: 300,
 
 },
-}
+
+{
 
 category: 'World Leaders',
 question: 'The 1971 Ugandan coup d\'etat was led by which military general who later became President? A. Idi Amin B. Mummar Gaddafi C. Milton Obote D. Yoweri Mudseveni',
@@ -72,7 +79,8 @@ answer: 'A. Idi Amin',
 points: 400,
 
 },
-}
+
+{
 
 category: 'World Leaders',
 question: 'Which Tunisian President was ousted during the "Arab Spring"? A. Hosni Mubarak B. Habib Bourguiba C. Zine El Abidine Ben Ali D. Beji Caid Essebsi',
@@ -80,7 +88,9 @@ answer: 'C. Zine El Abidine Ben Ali',
 points: 500,
 
 },
-}
+
+
+{
 
 category: 'Film Directors',
 question: 'Which director filmed the 1972 film "The Godfather"? A. Michael Bay B. Francis Ford Coppola C. Ron Howard D. Orson Welles',
@@ -88,7 +98,8 @@ answer: 'B. Francis Ford Coppola',
 points: 100,
 
 },
-}
+
+{
 
 category: 'Film Directors',
 question: '"E.T. the Extra-Terrestrial was directed by which American film maker? A. Steven Speilberg B. James Cameron C. Tim Burton D. George Lucas',
@@ -96,7 +107,8 @@ answer: 'A. Steven Speilberg',
 points: 200,
 
 },
-}
+
+{
 
 category: 'Film Directors',
 question: '"2001: A Space Odyssey" was made by which American film maker? A. Martin Scorsese B. Robert Altman C. Quentin Tarantino D. Stanley Kubrick',
@@ -104,7 +116,8 @@ answer: 'D. Stanley Kubrick',
 points: 300,
 
 },
-}
+
+{
 
 category: 'Film Directors',
 question: 'Which Italian film maker known for his fantasy and unique style directed the film "La Dolce Vita"? A. Roberto Rossellini B. Michelangelo Antonioni C. Federico Fellini D. Bernardo Bertolucci',
@@ -112,7 +125,8 @@ answer: 'C. Federico Fellini',
 points: 400,
 
 },
-}
+
+{
 
 category: 'Film Directors',
 question: 'The movie "Dreams" was the last film of which famous Japanese director? A. Takashi Miike B. Akira Kurosawa C. Kenji Mizoguchi D. Shohei Imamura',
@@ -120,7 +134,8 @@ answer: 'B. Akira Kurosawa',
 points: 500,
 
 },
-}
+
+{
 
 category: 'Mythology',
 question: 'Which Greek goddess was knowns as goddess of beauty, love, desire, and pleasure? A. Andromeda B. Helen of Troy C. Athena D. Aphrodite',
@@ -128,7 +143,8 @@ answer: 'D. Aphrodite',
 points: 100,
 
 },
-}
+
+{
 
 category: 'Mythology',
 question: 'Which Greek god was known as the god of boundaries, travel, communication, trade, language, and writing? A. Hermes B. Hades C. Apollo D. Zeus',
@@ -136,7 +152,9 @@ answer: 'A. Hermes',
 points: 200,
 
 },
-}
+
+
+{
 
 category: 'Mythology',
 question: 'Which Egyptian deity\'s prominent role was maintaining the universe? A. Isis B. Ma\'at C. Anubis D. Thoth',
@@ -144,7 +162,8 @@ answer: 'Thoth',
 points: 300,
 
 },
-}
+
+{
 
 category: 'Mythology',
 question: 'Which Roman god is known as the god of the sky and thunder? A. Jupiter B. Mars C. Vulcan D. Mercury',
@@ -152,7 +171,9 @@ answer: 'A. Jupiter',
 points: 400,
 
 },
-}
+
+
+{
 
 category: 'Mythology',
 question: 'Which Roman god is depicted as having two-faces and is known as the god of beginnings, gates, transitions, time, duality, passages, and endings? A. Minerva B. Ceres C. Janus D. Vesta',
@@ -160,7 +181,9 @@ answer: 'C. Janus',
 points: 500,
 
 },
-}
+
+
+{
 
 category: 'Philosophy',
 question: 'Which Greek philosopher wrote "The Republic"? A. Plato B. Aristotle C. Socrates D. Plutarch',
@@ -168,7 +191,8 @@ answer: 'A. Plato',
 points: 100,
 
 },
-}
+
+{
 
 category: 'Philosophy',
 question: 'The book "Thus Spoke Zarathustra" was written by which philosopher? A. Immanuel Kant B. Rene Descartes C. Friedrich Nietzche D. Soren Kierkegaard',
@@ -176,7 +200,8 @@ answer: 'C. Friedrich Nietzche',
 points: '200',
 
 },
-}
+
+{
 
 category: 'Philosophy',
 question: 'Which French philosopher is known for their work in post-structuralism and postmodern philosophy? A. Simone de Beauvoir B. Voltaire C. Jean-Paul Sartre D. Jacques Derrida',
@@ -184,7 +209,8 @@ answer: 'D. Jacques Derrida',
 points: 300,
 
 },
-}
+
+{
 
 category: 'Philosophy',
 question: 'Which artist is known for his painting "The School of Athens" which depicts nearly every great Greek philosopher? A. El Greco B. Michelangelo C. Caravaggio D. Raphael',
@@ -192,7 +218,8 @@ answer: 'D. Raphael',
 points: 400,
 
 },
-}
+
+{
 
 category: 'Philosophy',
 question: 'Which philosopher who is known primarily for his work in logic, mathematics, and linguistics and published the book "Tractatus Logico-Philosophicus"? A. Karl Marx B. Friedrich Engels C. Ludwig Wittgenstein D. Bertrand Russel',
@@ -200,8 +227,31 @@ answer: 'C. Ludwig Wittgenstein',
 points: 500,
 
 },
+
+];
+
+
+// ALL INSIDE CLICK EVENT CALLBACK
+
+// 1) pull out id value from the event.target or $(this) using jquery
+// or whatever
+
+// now initiate this loop (still inside callback)
+// purpose: iterate over every element (question object) inside questions array
+// stop when you find the question whose 'questionId' value is equal to the id of the 
+// thing that was clicked
+for (var i = 0; i < question.length; i++) {
+	if (clickedId === question[i].questionId) {
+		// do something and break out of loop
+		//assign the question object you found to a variable
+	}
 }
 
-]  
+// NOW OUTSIDE loop
+// use the info from the question variable (i.e. question, category, answer, points, etc)
+// utilize that to change info in your modal that pops up the question
+
+
+
 
 
